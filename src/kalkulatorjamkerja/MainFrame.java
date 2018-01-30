d944aa2a73f7aa305194cc81f81c17a3e2e8c96c
damarfitz29 <35796794+damarfitz29@users.noreply.github.com>
2018-01-30 09:13:36 +0700

    Update GUI

--- src/kalkulatorjamkerja/MainFrame.java
+++ src/kalkulatorjamkerja/MainFrame.java
@@ -58,7 +58,6 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtBulan = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
@@ -67,32 +66,38 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         txtJumlah = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
+        lblBack = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
-        jLabel1.setText("KALKULATOR JAM KERJA");
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel2.setText("Bulan");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 50, -1, 30));
 
+        txtBulan.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        txtBulan.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtBulanFocusGained(evt);
+            }
+        });
         txtBulan.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtBulanActionPerformed(evt);
             }
         });
-        txtBulan.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txtBulanKeyReleased(evt);
-            }
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                txtBulanKeyTyped(evt);
+        getContentPane().add(txtBulan, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 50, 34, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel3.setText("Tahun");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 50, -1, 30));
+
+        txtTahun.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtTahunFocusGained(evt);
             }
         });
-
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel3.setText("Tahun");
-
         txtTahun.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtTahunActionPerformed(evt);
@@ -103,6 +108,7 @@ public class MainFrame extends javax.swing.JFrame {
                 txtTahunKeyTyped(evt);
             }
         });
+        getContentPane().add(txtTahun, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 50, 59, -1));
 
         btnHitung.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btnHitung.setText("HITUNG");
@@ -111,84 +117,36 @@ public class MainFrame extends javax.swing.JFrame {
                 btnHitungActionPerformed(evt);
             }
         });
+        getContentPane().add(btnHitung, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 90, 80, -1));
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel4.setText("Jumlah Hari Kerja");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, 30));
 
         txtJumlah.setEditable(false);
-        txtJumlah.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        txtJumlah.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        getContentPane().add(txtJumlah, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 130, 50, -1));
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel5.setText("Jam");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 30, 30));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(btnHitung)
-                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                            .addComponent(jLabel2)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(txtBulan, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(18, 18, 18)
-                            .addComponent(jLabel3)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(txtTahun, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addGap(18, 18, 18)
-                        .addComponent(txtJumlah, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel5)))
-                .addContainerGap(27, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(49, 49, 49))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(txtBulan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3)
-                    .addComponent(txtTahun, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(btnHitung)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtJumlah, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel4))
-                .addContainerGap(26, Short.MAX_VALUE))
-        );
+        lblBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/kalkulatorjamkerja/back kal.png"))); // NOI18N
+        lblBack.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblBackMouseClicked(evt);
+            }
+        });
+        getContentPane().add(lblBack, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtBulanKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBulanKeyTyped
-        // TODO add your handling code here:
-        FilterHanyaAngka(evt);
-    }//GEN-LAST:event_txtBulanKeyTyped
-
     private void txtTahunKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtTahunKeyTyped
         // TODO add your handling code here
         FilterHanyaAngka(evt);
     }//GEN-LAST:event_txtTahunKeyTyped
-
-    private void txtBulanKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBulanKeyReleased
-        // TODO add your handling code here:
-        FilterNol(evt);
-    }//GEN-LAST:event_txtBulanKeyReleased
 
     /**
      * Method Action Button Hitung.
@@ -210,15 +168,32 @@ public class MainFrame extends javax.swing.JFrame {
         txtJumlah.setText(Integer.toString(jumlah_jk));
     }//GEN-LAST:event_btnHitungActionPerformed
 
+    private void txtTahunActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTahunActionPerformed
+        // TODO add your handling code here:
+        btnHitungActionPerformed(evt);
+    }//GEN-LAST:event_txtTahunActionPerformed
+
     private void txtBulanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBulanActionPerformed
         // TODO add your handling code here:
         txtTahun.requestFocus();
     }//GEN-LAST:event_txtBulanActionPerformed
 
-    private void txtTahunActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTahunActionPerformed
+    private void lblBackMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBackMouseClicked
         // TODO add your handling code here:
-        btnHitungActionPerformed(evt);
-    }//GEN-LAST:event_txtTahunActionPerformed
+        txtBulan.setText("ex:2");
+        txtTahun.setText("ex:2018");
+        lblBack.requestFocus();
+    }//GEN-LAST:event_lblBackMouseClicked
+
+    private void txtBulanFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtBulanFocusGained
+        // TODO add your handling code here:
+        txtBulan.setText("");
+    }//GEN-LAST:event_txtBulanFocusGained
+
+    private void txtTahunFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtTahunFocusGained
+        // TODO add your handling code here:
+        txtTahun.setText("");
+    }//GEN-LAST:event_txtTahunFocusGained
 
     /**
      * @param args the command line arguments
@@ -257,11 +232,11 @@ public class MainFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnHitung;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel lblBack;
     private javax.swing.JTextField txtBulan;
     private javax.swing.JTextField txtJumlah;
     private javax.swing.JTextField txtTahun;
