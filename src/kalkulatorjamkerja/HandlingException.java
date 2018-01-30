9f64b44fe29c48e97d572abe3c1499e01b48bf74
Yusuf Hanafi Angkat <yusufhanafiangkat@gmail.com>
2018-01-30 09:14:42 +0700

    set

---
+++ src/kalkulatorjamkerja/HandlingException.java
@@ -0,0 +1,18 @@
+package kalkulatorjamkerja;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author YusufHA
+ */
+public class HandlingException {
+    public static void main(String[] args) {
+//        try {
+//            CekHari ch = new CekHari();
+//            ch.countSSRK(0, 0);
+//        } catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+    }
+}
